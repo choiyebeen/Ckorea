7eea286ca950dbc80ab26c6ada0a3a1d34bad562
dpqlsdl8@naver.com <dpqlsdl8@naver.com>
2022-04-23 11:50:58 +0900

    수업 내용

---
+++ Ckorea/Cproject02.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+void main2() {
+	printf("작성자 : 최예빈");
+	printf("\n날짜 : 2022-04-23");
+	printf("\n기능 : printf 사용하기");
+}
---
+++ Ckorea/Cproject03.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void main() {
+	//printf("Hello");
+
+
+
+
+
+
+	char var1 = 'a';
+	int var2 = 3;
+	double var3 = 3.14;
+	char var4[256] = "안녕하세요, 반갑습니다.";
+
+	printf("숫자는 %d \n", var2);
+	printf("문자는 %c \n", var1);
+	printf("소수점 있는 숫자는 %f \n", var3);
+	printf("문자열은 %s! \n", var4);
+
+	int num; // 변수 선언
+	num = 5; // 대입
+	num = 1;
+	printf("%d", num);
+
+	int num2 = 311111;
+}
